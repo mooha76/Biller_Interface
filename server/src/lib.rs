c84709a90428a69179c26df3476f500dfb01028c
Mohamed Farah <Mofarahali76@gmail.com>
2023-12-27 16:05:58 +0300

    initializing app and server pacages

---
+++ server/src/lib.rs
@@ -0,0 +1,37 @@
+use axum::{
+    routing::get,
+    Router,
+};
+use config::AppConfig;
+use errors::AppResult;
+use initialize::InitializeApp;
+use std::net::TcpListener;
+use tracing::info;
+
+pub struct Server {
+    pub initialize: InitializeApp,
+    pub listener: TcpListener,
+}
+
+impl Server {
+    pub async fn new(mut config: AppConfig) -> AppResult<Self> {
+        let listener = TcpListener::bind(config.server.get_addr())?;
+        config.server.port = listener.local_addr()?.port();
+        let state = InitializeApp::new(config).await?;
+        Ok(Self { initialize: state, listener })
+    }
+
+    pub async fn run(self) -> std::io::Result<()> {
+        info!(
+            "run server: {} profile: {}",
+            self.initialize.config.server.get_addr(),
+            self.initialize.config.profile
+        );
+
+        let app = Router::new()
+            .route("/", get(|| async { "Hello, World!" }));
+
+        let listener = tokio::net::TcpListener::bind(&self.listener).await.unwrap();
+        axum::serve(listener, app).await
+    }
+}
